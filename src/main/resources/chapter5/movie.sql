7a07014b43465f5f59a004a851d4516cdde4639c
songminkyu <rabbircarrot@naver.com>
2025-11-14 21:16:55 +0900

    chapter5 sql 초기 데이터 삽입 쿼리 추가

---
+++ src/main/resources/chapter5/movie.sql
@@ -0,0 +1,11 @@
+INSERT INTO portfolio.ch5_movie (title) VALUES
+                                            ('The Shawshank Redemption'),
+                                            ('The Godfather'),
+                                            ('The Dark Knight'),
+                                            ('Pulp Fiction'),
+                                            ('Forrest Gump'),
+                                            ('Inception'),
+                                            ('The Matrix'),
+                                            ('Interstellar'),
+                                            ('Parasite'),
+                                            ('Oppenheimer');
