dd16e532557162427e42c3ea07d07c436f2fa218
unknown <rabbircarrot@naver.com>
2025-11-21 23:19:32 +0900

    Update `BookPracticeServiceTest` with detailed cache hit/miss comments

--- src/test/java/ding/co/backendportfolio/chapter6/_2_remote_redis_practice/BookPracticeServiceTest.java
+++ src/test/java/ding/co/backendportfolio/chapter6/_2_remote_redis_practice/BookPracticeServiceTest.java
@@ -38,11 +38,18 @@ class BookPracticeServiceTest {
     @Test
     void findBookById() throws JsonProcessingException {
         // 첫 번째에는 캐시가 없어 DB 의 데이터를 가져와서 사용한다.
+        // id = 1
+        // cache.get("remote-cache-practice:1")
+        // -> ?
+        // -> CACHE MISS // 키가 존재 하지 않기때문에.
+        // -> db.get("1") DB에서 "1" 가져오고
+        // -> CACHE PUT // DB에서 가져온 값을 캐시 업데이트 진행
         Book firstBook = bookPracticeService.findBookById(this.book.getId());
 
         log.info("--------------------");
 
         // 두 번째에는 캐시의 데이터를 이용한다.
+        // cache.get("remote-cache-practice:1")  -> ? -> CACHE HIT
         Book secondBook = bookPracticeService.findBookById(this.book.getId());
 
         assertThat(firstBook.getId()).isEqualTo(secondBook.getId());
